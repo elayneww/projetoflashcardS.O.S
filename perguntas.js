703c4ba24155b357fbabf5fa23513266a280d121
elayneww <140831189+elayneww@users.noreply.github.com>
2024-10-05 20:26:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,19 @@
+criacartao(
+    'Psicologia',
+    'Quais fatores psicológicos e sociais contribuem para a formação de uma mente fechada?',
+    'Educação rígida, experiências traumáticas, medo do desconhecido e traços de personalidade como baixo grau de abertura.'
+)
+criacartao(
+    'Psicologia',
+    'De que maneira a mente fechada pode impactar relacionamentos interpessoais?',
+    'Dificulta comunicação e empatia, resultando em mal-entendidos e isolamento.'
+)
+criacartao(
+    'Psicologia',
+    'Quais são os efeitos da mente fechada na tomada de decisões',
+    'Limita a consideração de alternativas e inovações, levando a escolhas subótimas.'
+)
+criacartao(
+    'Psicologia',
+    
+)
